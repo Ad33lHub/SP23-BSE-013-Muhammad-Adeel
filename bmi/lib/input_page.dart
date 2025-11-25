726945753d58f8bcd2eff09d9f1d92ce9164a942
Ad33lHub <auniversity577@gmail.com>
2025-11-25 21:19:23 +0500

    video2 9theme apply
    apply dark theme

---
+++ bmi/lib/input_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Input Page")),
+      body: Center(child: Text("Your content here")),
+    );
+  }
+}
