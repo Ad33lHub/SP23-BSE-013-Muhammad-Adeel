8b98f0d7517f43e3835eebdb008cff65d4cff3c1
Ad33lHub <auniversity577@gmail.com>
2025-11-27 20:39:17 +0500

    Flutter in Urdu | BMI Use Gesture Detector Lecture 41

--- bmi/lib/input_page.dart
+++ bmi/lib/input_page.dart
@@ -1,13 +1,33 @@
+// ignore_for_file: non_constant_identifier_names, library_private_types_in_public_api
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'icontext.dart';
 import 'container.dart';
+
+// ignore: use_key_in_widget_constructors
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
 }
 
+const activecolor = Color.fromARGB(255, 2, 5, 23);
+const deactivecolor = Color.fromARGB(255, 3, 9, 40);
+
 class _InputPageState extends State<InputPage> {
+  Color maleColor = deactivecolor;
+  Color femaleColor = deactivecolor;
+
+  void UpdateColor(int gender) {
+    if (gender == 1) {
+      maleColor = activecolor;
+      femaleColor = deactivecolor;
+    } else if (gender == 2) {
+      femaleColor = activecolor;
+      maleColor = deactivecolor;
+    } else {}
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,47 +38,59 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: RepeatContainerCode(
-                      colour:Color.fromARGB(255, 3, 9, 40),
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        UpdateColor(1);
+                      });
+                    },
+                    child: RepeatContainerCode(
+                      colour: maleColor,
                       cardWidget: CardWidget(
                         iconData: FontAwesomeIcons.male,
                         label: 'Male',
                       ),
+                    ),
                   ),
                 ),
                 Expanded(
-                  child: RepeatContainerCode(
-                    colour:Color.fromARGB(255, 3, 9, 40),
-                    cardWidget: CardWidget(
-                      iconData: FontAwesomeIcons.female,
-                      label: 'Female',
-                    ),),
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        UpdateColor(2);
+                      });
+                    },
+                    child: RepeatContainerCode(
+                      colour: femaleColor,
+                      cardWidget: CardWidget(
+                        iconData: FontAwesomeIcons.female,
+                        label: 'Female',
+                      ),
+                    ),
+                  ),
                 ),
               ],
             ),
           ),
           Expanded(
             flex: 2,
-            child: RepeatContainerCode(
-                colour:Color.fromARGB(255, 3, 9, 40),
-
-            ),
+            child: RepeatContainerCode(colour: Color.fromARGB(255, 3, 9, 40)),
           ),
           Expanded(
             child: Row(
               children: <Widget>[
                 Expanded(
                   child: RepeatContainerCode(
-                      colour:Color.fromARGB(255, 3, 9, 40),
-                      cardWidget: CardWidget(
-                        iconData: FontAwesomeIcons.male,
-                        label: 'Male',
-                      ),
+                    colour: Color.fromARGB(255, 3, 9, 40),
+                    cardWidget: CardWidget(
+                      iconData: FontAwesomeIcons.male,
+                      label: 'Male',
+                    ),
                   ),
                 ),
                 Expanded(
                   child: RepeatContainerCode(
-                    colour:Color.fromARGB(255, 3, 9, 40),
+                    colour: Color.fromARGB(255, 3, 9, 40),
                     cardWidget: CardWidget(
                       iconData: FontAwesomeIcons.female,
                       label: 'Female',
@@ -73,6 +105,3 @@ class _InputPageState extends State<InputPage> {
     );
   }
 }
-
-
-
